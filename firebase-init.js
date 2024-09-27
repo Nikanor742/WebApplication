7eb8bf4f5ea479cef69470d94b391791b8522777
Nikanor <nik14kg@gmail.com>
2024-09-27 14:29:46 +0700

    123

---
+++ firebase-init.js
@@ -0,0 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/9.x.x/firebase-database.js"></script>
+
+<script>
+    const firebaseConfig = {
+        apiKey: "AIzaSyAUXYPyxl2PGTYg3s5qGQWRFX2m91fKMcM",
+        authDomain: "webapplication-efaea.firebaseapp.com",
+        databaseURL: "https://webapplication-efaea-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "webapplication-efaea",
+        storageBucket: "webapplication-efaea.appspot.com",
+        messagingSenderId: "19816069355",
+        appId: "1:19816069355:android:b190aa7f61bbd39ab9a6ae"
+    };
+
+    // Инициализация Firebase
+    firebase.initializeApp(firebaseConfig);
+</script>
